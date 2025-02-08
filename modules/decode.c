58e292b942dfb440b3c76acb8f2c1aef41cf82d7
yan7-psu <yan7@pdx.edu>
2025-02-07 20:42:43 -0800

    Update decode.c
    add j-immediate

--- modules/decode.c
+++ modules/decode.c
@@ -60,16 +60,25 @@ void decode_instruction(uint32_t instruction) {
             printf("imm: 0x%05x, rd: %u\n", decoded.imm, decoded.rd);
             break;
 
-        case 0x6F:  // jump
-            
+        case 0x6F:  //jal - j-type          
             decoded.imm = (((instruction >> 31) & 0x1) << 19) | (((instruction >> 21) & 0x3FF) << 1) | (((instruction >> 20) & 0x1) << 11) | (((instruction >> 12) & 0xFF) << 12);
             decoded.rd = (instruction >> 7) & 0x1F;
             printf("J type instuction:\n");
             printf("imm: 0x%05x, rd: %u\n", decoded.imm, decoded.rd);
             break;
 
+        case 0x67: //jalr - jump with i-type encoding
+            decoded.imm = (int32_t)(instruction >> 20);  // Sign extended 12 bit immediate
+            decoded.rs1 = (instruction >> 15) & 0x1F;
+            decoded.funct3 = (instruction >> 12) & 0x7;
+            decoded.rd = (instruction >> 7) & 0x1F;
+            printf("I type instruction:\n");
+            printf("imm: 0x%03x, rs1: %u, funct3: 0x%01x, rd: %u\n", decoded.imm, decoded.rs1, decoded.funct3, decoded.rd);
+            break;
+
+
         default:
             printf("Invalid opcode: 0x%02x\n", opcode);
             break;
     }
-}
+}       
