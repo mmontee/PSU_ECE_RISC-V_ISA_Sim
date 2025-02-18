7b9683d72623af333956a3e18719182f785a4714
BhavanaSrinivasegowda <bhavanas@pdx.edu>
2025-02-18 14:02:05 -0800

    Update instructions.c

--- modules/instructions.c
+++ modules/instructions.c
@@ -100,65 +100,87 @@ void and_r(decoded_instr_t instruction, uint32_t registers[])
 void addi(decoded_instr_t instruction, uint32_t registers[]) //rd = rs1 + imm
 {
         registers[instruction.rd] = registers[instruction.rs1] + instruction.imm;
-        printf("Executed ADDI: rd=%u, rs1=%u, imm=%d\n", instruction.rd, instruction.rs1, instruction.imm);
+        #ifdef DEBUG
+            printf("Executed ADDI: rd=%u, rs1=%u, imm=%d\n", instruction.rd, instruction.rs1, instruction.imm);
+        #endif
 }
 
 void slti(decoded_instr_t instruction, uint32_t registers[]) //rd = (rs1 < imm)?1:0
 {
         registers[instruction.rd] = (registers[instruction.rs1] < instruction.imm) ? 1 : 0;
-        printf("Executed SLTI: rd=%u, rs1=%u, imm=%d\n", instructoin.rd, instruction.rs1, instruction.imm);
+        #ifdef DEBUG
+            printf("Executed SLTI: rd=%u, rs1=%u, imm=%d\n", instructoin.rd, instruction.rs1, instruction.imm);
+        #endif
 }
 
 void sltiu(decoded_instr_t instruction, uint32_t registers[]) //rd = (rs1 < imm)?1:0 zero extends
 {
         registers[instruction.rd] = (registers[instruction.rs1] < (uint32_t)instruction.imm ? 1 : 0;
-        printf("Executed SLTIU: rd=%u, rs1=%u, imm=%u\n", instruction.rd, instruction.rs1, (uint32_t)instruction.imm);
+        #ifdef DEBUG
+            printf("Executed SLTIU: rd=%u, rs1=%u, imm=%u\n", instruction.rd, instruction.rs1, (uint32_t)instruction.imm);
+        #endif
 }
 
 void xori(decoded_instr_i instruction, uint32_t registers[]) //rd = rs1 ^ imm
 {
         registers[instruction.rd] = registers[instruction.rs1] ^ instruction.imm;
-        printf("Executed XORI: rd=%u, rs1=%u, imm=%d\n", instruction.rd, instruction.rs1, instruction.imm);
+        #ifdef DEBUG
+            printf("Executed XORI: rd=%u, rs1=%u, imm=%d\n", instruction.rd, instruction.rs1, instruction.imm);
+        #endif
 }
 
 void ori(decoded_instr_t instruction, uint32_t registers[]) //rd = rs1 | imm
 {
         registers[instruction.rd] = registers[instruction.rs1] | instruction.imm;
-        printf("Executed ORI: rd=%u, rs1=%u, imm=%d\n", instruction.rd, instruction.rs1, instruction.imm);
+        #ifdef DEBUG
+            printf("Executed ORI: rd=%u, rs1=%u, imm=%d\n", instruction.rd, instruction.rs1, instruction.imm);
+        #endif
 }
 
 void andi(decoded_instr_t instruction, uint32_t registers[]) //rd = rs1 & imm
 {
         registers[instruction.rd] = registers[instruction.rs1] & instruction.imm;
-        printf("Executed ANDI: rd=%u, rs1=%u, imm=%d\n", instruction.rd, instruction.rs1, instruction.imm);
+        #ifdef DEBUG
+            printf("Executed ANDI: rd=%u, rs1=%u, imm=%d\n", instruction.rd, instruction.rs1, instruction.imm);
+        #endif
 }
 
 void slli(decoded_instr_t instruction, uint32_t registers[])
 {
         registers[instruction.rd] = registers[instruction.rs1] << (instruction.imm & 0x1F);
-        printf("Executed SLLI: rd=%u, rs1=%u, imm%d\n", instruction.rd, instruction.rs1, instruction.imm);
+        #ifdef DEBUG
+            printf("Executed SLLI: rd=%u, rs1=%u, imm%d\n", instruction.rd, instruction.rs1, instruction.imm);
+        #endif
 }
 
 void srli(decoded_instr_t instruction, uint32_t registers[])
 {
         registers[instruction.rd] = registers[instruction.rs1] >> (instruction.imm & 0x1F);
-        printf("Executed SRLI: rd=%u, rs1=%u, imm=%d\n", instruction.rd, instruction.rs1, instruction.imm);
+        #ifdef DEBUG
+            printf("Executed SRLI: rd=%u, rs1=%u, imm=%d\n", instruction.rd, instruction.rs1, instruction.imm);
+        #endif
 }
 
 void srai(decoded_instr_t instruction, uint32_t registers[])
 {
         registers[instruction.rd] = (int32_t)registers[instruction.rs1] >> (instruction.imm & 0x1F);
-        printf("Executed SRAI: rd=%u, rs1=%u, imm=%d\n", instruction.rd, instruction.rs1, instruction.imm);
+        #ifdef DEBUG
+            printf("Executed SRAI: rd=%u, rs1=%u, imm=%d\n", instruction.rd, instruction.rs1, instruction.imm);
+        #endif
 }
 
 void lui(decoded_instr_t instruction, uint32_t registers[]) //rd = imm << 12
 {
         registers[instruction.rd] = instruction.imm << 12;
-        printf("Executed LUI: rd=%u, imm=0x%08x\n", instruction.rd, instruction.imm);
+        #ifdef DEBUG
+            printf("Executed LUI: rd=%u, imm=0x%08x\n", instruction.rd, instruction.imm);
+        #endif
 }
 
 void auipc(decoded_instr_t instruction, uint32_t registers[], uint32_t programCounter) //rd = PC + (imm << 12)
 {
         registers[instruction.rd] = programCounter + (instruction.imm << 12);
-        printf("Executed AUIPC: rd=%u, imm=0x%08x, programCounter=0x%08\n", instruction.rd, instruction.imm, programCounter);
+        #ifdef DEBUG
+            printf("Executed AUIPC: rd=%u, imm=0x%08x, programCounter=0x%08\n", instruction.rd, instruction.imm, programCounter);
+        #endif
 }
